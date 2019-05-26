d56ab9b8a3730d9c5dce636c87444dae59d40d80
toukagarry <48177192+toukagarry@users.noreply.github.com>
2019-05-26 13:33:39 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,21 +2,30 @@ const Discord = require('discord.js');
 
 module.exports.run = async (client, message, args) => {   
   let embed = new Discord.RichEmbed()
-      .setTitle("NogameNolife Help")
+      .setTitle("NogameNolife")
       .setDescription("Use `help <command>` for details.")
-      .setColor(0xFF4500)
+      .setColor(0xba4b5b)
       .addField("Legend", "`<arg>` Compulsory argument\n`[arg]` Optional argument");
   
   let generalArr = [],
+      musicArr = [],
+      funArr = [],
+      moderationArr = [],
       debugArr = [];
       //otherArr = [];
         
   client.cmdhelp.filter(cmd => cmd.category === "General").forEach((cmd) => {generalArr.push(cmd.name)});
+  client.cmdhelp.filter(cmd => cmd.category === "Music").forEach((cmd) => {musicArr.push(cmd.name)});
+  client.cmdhelp.filter(cmd => cmd.category === "Moderation").forEach((cmd) => {moderationArr.push(cmd.name)});
+  client.cmdhelp.filter(cmd => cmd.category === "Fun").forEach((cmd) => {funArr.push(cmd.name)});
   client.cmdhelp.filter(cmd => cmd.category === "Debug").forEach((cmd) => {debugArr.push(cmd.name)});
   //client.cmdhelp.filter(cmd => cmd.category === 'Other').forEach((cmd) => {otherArr.push(cmd.name)});
   
   embed
     .addField("General", generalArr.map(g => g).join('\n'), true)
+    .addField("Music", musicArr.map(g => g).join('\n'), true)
+    .addField("Moderation", moderationArr.map(g => g).join('\n'), true)
+    .addField("Fun", funArr.map(g => g).join('\n'), true)
     .addField("Debug", debugArr.map(g => g).join('\n', true));
   
   if (!args[0]) {
@@ -24,7 +33,7 @@ module.exports.run = async (client, message, args) => {
   } else {
     let cmd = client.cmdhelp.filter(cmd => cmd.name === args[0]).first();
     let cmdEmbed = new Discord.RichEmbed()
-      .setColor(0xFF4500);
+      .setColor(0xba4b5b);
     
     if (!cmd) {
       embed
